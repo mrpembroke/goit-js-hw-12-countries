e866e98d5a96b13d8f32e4563f1de8044c81c568
Dmytro Petrenko <blinker182ca@gmail.com>
2020-11-14 10:55:38 +0200

    add items refs

---
+++ src/js/get-refs.js
@@ -0,0 +1,5 @@
+export const refs = {
+    searchInput: document.querySelector('.js-input'),
+    cardContainer: document.querySelector('.js-card-container'),
+    listContainer: document.querySelector('.js-countries-list')
+}
